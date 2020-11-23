505e86c7b5d6aedd0ec576d531d85adf73fda6a4
IlyaGrytsatsuev <ilya.gr2003@yandex.ru>
2020-11-24 01:03:59 +0300

    1

--- additionlab2v4/main.c
+++ additionlab2v4/main.c
@@ -1,15 +1,43 @@
-//
-//  main.c
-//  additionlab2v4
-//
-//  Created by gru gri on 23.10.2020.
-//  Copyright © 2020 gru gri. All rights reserved.
-//
-
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main ()
+{
+    char c;
+    int state = 0, num_chars = 0, k;
+    char a[1000];
+    printf("Enter the the length :\n");
+    scanf("%d",&k);
+    printf("Enter the sentence:\n ");
+    getchar();
+    while (((c = getchar()) != EOF) && (c != '\n'))
+    {
+        if (('a' <= c &&c <= 'z')||('A' <= c && c <= 'Z')){
+            state = 1;
+            a[num_chars] = c;
+            num_chars++;
+        }
+        else
+            if(state == 1 )
+            {
+                if (num_chars == k)
+                {
+                    for (int i=0;i<num_chars;i++){
+                        printf("%c", a[i]);
+                    }
+                    printf("\n");
+                }
+                state = 0;
+                num_chars = 0;
+                }
+    }
+    if (state == 1 && num_chars == k)
+    {
+        for (int i=0;i<num_chars;i++){
+            printf("%c", a[i]);
+        }
+        printf("\n");
+        num_chars = 0;
     return 0;
+    }
 }
+
+ 
